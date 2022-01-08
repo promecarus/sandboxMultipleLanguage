61378abfa7023e40f8d9cfdb89224669e167b558
promecarus <haikalslipi@gmail.com>
2022-01-08 21:54:01 +0700

    yg wajib selesai

--- _so/praktikum3/tugas/no6.sh
+++ _so/praktikum3/tugas/no6.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 # Buat program yang menghapus file disebuah direktori jika file itu mengandung bad karakter (/[\+\{\;\"\\\=\?~\(\)\<\>\&\*\|\$]\}\`)
 
-# FILES=$(ls *.*)
-# BADCHARS=([ \ + \ { \ ; \ " \ \ \ = \ ? ~ \ ( \ ) \ < \ > \& \ * \ | \ $ ] \ } \ `)
-# for FILE in $FILES; do
-#     if [[ $FILE =~ [\+\{\;\"\\\=\?~\(\)\<\>\&\*\|\$]\}\` ]]; then
-#         rm -r $FILE
-#     fi
-# done
+# penghitungan mundur sebelum file dihapus, user masih bisa membatalkannya dalam waktu 10 detik
+for (( i=10; i>-1; i-- )); do
+    clear
+    echo -e "Semua file yang mengandung bad karakter akan dihapus dalam $i detik.\n[Tekan CTRL+C untuk membatalkan]"
+    sleep 1
+done
 
-FILES=($(ls *\#*) $(ls *&*))
-echo $FILES
+clear
+FILES=$(ls *.*)
+for FILE in $FILES; do
+    if [[ $FILE =~ [^[:alnum:]] && ! $FILE =~ [on] ]]; then
+        echo "File "$FILE" mengandung bad karakter"
+        rm $FILE
+        echo "File "$FILE" telah dihapus"
+    fi
+done

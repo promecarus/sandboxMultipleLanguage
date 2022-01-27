a67db60a7363a3df993e7b0091095ebbcdb1af9f
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-01-27 10:14:58 +0700

    freeCodeCamp Regular Expressions done
    hwehwehwe

---
+++ freeCodeCamp/js/Regular Expressions/extractMatches.js
@@ -0,0 +1,7 @@
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+// info
+// "string".match(/regex/);
+// /regex/.test("string");

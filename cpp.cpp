557c0118eff5f915a6e98653a448f3e2c3a72a6c
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-03-10 13:46:32 +0700

    anjae, sok animasi ketik da lu

--- cpp.cpp
+++ cpp.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <array>
 #include <algorithm>
+#include <chrono>
+#include <thread>
 
 using namespace std;
+using namespace chrono;
+using namespace this_thread;
+
+void animasiKetik (int i, int time, string batas) {
+	cout << i;
+	sleep_for(milliseconds(time));
+	cout << batas;
+	sleep_for(milliseconds(time));
+}
 
 int main() {
+
 	array <int, 10> angkaAngka = {9, 4, 6, 2, 0, 7, 3, 1, 5, 8};
 	for(int i: angkaAngka) {
-		cout << i << " ";
+		animasiKetik(i, 100, ";");
 	}
 	cout << "\n";
 	sort(angkaAngka.begin(), angkaAngka.end());
 	for(int i: angkaAngka) {
-		cout << i << " ";
+		animasiKetik(i, 100, ";");
 	}
 	cout << "\n";
 

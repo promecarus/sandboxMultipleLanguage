50096d250553326424aef84b382c56e7bef3b843
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-03-22 18:48:01 +0700

    add folder _pwd

--- cpp.cpp
+++ cpp.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 // #include <typeinfo>
 // #include <chrono>
 // #include <thread>
@@ -12,39 +13,25 @@ using namespace std;
 // using namespace this_thread;
 
 int main() {
-	// int n[10], total = 0;
-	// for (int i = 0; i < 10; i++) {
-	// 	cout << "Masukkan angka untuk n["<< i <<"]: ";
-	// 	cin >> n[i];
+	// char *name[] = {"Ali", "Ani", "Tono"};
+	// cout << name[0] << endl; 	
+	// name[0] = "Asep";
+	// cout << name[0] << endl; 	
+	// char name2[][10] = {"Ali", "Ani", "Tono"};
+	// name2[0] = "Ali"; 	
+	// int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	// for(int i = 0; i < 10; i++) {
+	// 	cout << arr[i] << " ";
 	// }
-	// int max = n[0], min = n[0];
-	// for(int i = 1; i < 10; i++) {
-	// 	if (n[i] > max) {
-	// 		max = n[i];
-	// 	}
-	// 	if (n[i] < min) {
-	// 		min = n[i];
-	// 	}
-	// 	total += n[i];
+	// arr[1] = 20;
+	// cout << endl;
+	// for(int i = 0; i < 10; i++) {
+	// 	cout << arr[i] << " ";
 	// }
-	// cout << "\n";
-	// cout << "Nilai terbesar adalah " << max << endl;
-	// cout << "Nilai terkecil adalah " << min << endl;
-	// cout << "Nilai rata-rata adalah " << total/10 << endl;
+	char str[] = "Hello World";
+	char * pch = strchr(str, 'W');
+	cout << pch << endl;
+	cout << pch-str << endl;
 
-	// string kata;
-	// cout << "Masukkan kata: ";
-	// cin >> kata;
-	// reverse(kata.begin(), kata.end());
-	// cout << kata;
-	int A[3][4] = {1, 3, 2, 4, 5, 7, 6, 8, 9, 11, 12};
-	int B[3][4] = {{1, 2}, {3, 4, 5}, {7}};
-
-	cout << "A[1][1] = " << A[1][1] << endl;
-	cout << "B[2][2] = " << B[2][2] << endl;
-	cout << "A[2][3] = " << A[2][3] << endl;
-	cout << "B[0][1] = " << B[0][1] << endl;
-	cout << "A[0][2] = " << A[0][2] << endl;
-	
 	return 0;
 }
--- kuliah/_asd/tugas02/nomor08.cpp
+++ kuliah/_asd/tugas02/nomor08.cpp
@@ -4,20 +4,24 @@
 using namespace std;
 
 int main() {
-	char *str = "Ini adalah sebuah sample string.";
-	cout << str << "\n";
+	// char *str = "Ini adalah sebuah sample string.";
+	// cout << str << "\n";
 
-	// strchr() mengembalikan karakter pertama dari depan, yang ada di dalam parameter kedua, sampai akhir string
-	// contoh penggunaan strchr()
-	cout << "Hasil strchr(str, 'a')\t\t: " << strchr(str, 'a') << "\n";
+	// // strchr() mengembalikan karakter pertama dari depan, yang ada di dalam parameter kedua, sampai akhir string
+	// // contoh penggunaan strchr()
+	// cout << "Hasil strchr(str, 'a')\t\t: " << strchr(str, 'a') << "\n";
 
-	// strrchr() mengembalikan karakter pertama dari belakang, yang ada di dalam parameter kedua, sampai akhir string
-	// contoh penggunaan strrchr()
-	cout << "Hasil strrchr(str, 'a')\t\t: " << strrchr(str, 'a') << "\n";
+	// // strrchr() mengembalikan karakter pertama dari belakang, yang ada di dalam parameter kedua, sampai akhir string
+	// // contoh penggunaan strrchr()
+	// cout << "Hasil strrchr(str, 'a')\t\t: " << strrchr(str, 'a') << "\n";
 
-	// strstr() mengembalikan kata pertama dari depan, yang ada di dalam parameter kedua, sampai akhir string
-	// contoh penggunaan strstr()
-	cout << "Hasil strstr(str, \"sample\")\t: " << strstr(str, "sample") << "\n";
+	// // strstr() mengembalikan kata pertama dari depan, yang ada di dalam parameter kedua, sampai akhir string
+	// // contoh penggunaan strstr()
+	// cout << "Hasil strstr(str, \"sample\")\t: " << strstr(str, "sample") << "\n";
+	char *contohString = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, magnam.";
+	cout << "strchr(contohString, 'i'): " << strchr(contohString, 'i') << "\n";
+	cout << "strrchr(contohString, 'i'): " << strrchr(contohString, 'i') << "\n";
+	cout << "strstr(contohString, \"dolor\"): " << strstr(contohString, "dolor") << "\n";
 
 	return 0;
 }

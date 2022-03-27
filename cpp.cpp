0d552077dfedb4bf3b3a53253115a8022257a4e0
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-03-27 11:38:00 +0700

    test mixing types cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <string>
-#include <string.h>
+// #include <string.h>
 // #include <typeinfo>
 // #include <chrono>
 // #include <thread>
 // #include <array>
-#include <algorithm>
+// #include <algorithm>
 // #include <bits/stdc++.h>
 
 using namespace std;
@@ -13,25 +12,13 @@ using namespace std;
 // using namespace this_thread;
 
 int main() {
-	// char *name[] = {"Ali", "Ani", "Tono"};
-	// cout << name[0] << endl; 	
-	// name[0] = "Asep";
-	// cout << name[0] << endl; 	
-	// char name2[][10] = {"Ali", "Ani", "Tono"};
-	// name2[0] = "Ali"; 	
-	// int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	// for(int i = 0; i < 10; i++) {
-	// 	cout << arr[i] << " ";
-	// }
-	// arr[1] = 20;
-	// cout << endl;
-	// for(int i = 0; i < 10; i++) {
-	// 	cout << arr[i] << " ";
-	// }
-	char str[] = "Hello World";
-	char * pch = strchr(str, 'W');
-	cout << pch << endl;
-	cout << pch-str << endl;
+	int a = 5;
+	double b = 2.0;
+	int c = 2;
 
+
+	cout << a / b << endl;
+	cout << a / c << endl;
+	system("pause");
 	return 0;
 }

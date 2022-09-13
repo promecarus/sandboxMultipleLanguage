868f0b33c980ab03cb9956f128b104456faa1658
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-09-13 15:43:41 +0700

    oop

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log(typeof typeof "string"); //string
+class Animal {
+	constructor(name, age, isMammal) {
+		(this.name = name), (this.age = age), (this.isMammal = isMammal);
+	}
+}
+
+class Rabbit extends Animal {
+	constructor(name, age) {
+		super(name, age, true);
+	}
+
+	eat() {
+		return `${this.name} sedang makan!`;
+	}
+}
+
+class Eagle extends Animal {
+	constructor(name, age) {
+		super(name, age, false);
+	}
+
+	fly() {
+		return `${this.name} sedang terbang!`;
+	}
+}
+
+const myRabbit = new Rabbit("Labi", 2);
+const myEagle = new Eagle("Elo", 4);
+
+console.log(myEagle.name);
